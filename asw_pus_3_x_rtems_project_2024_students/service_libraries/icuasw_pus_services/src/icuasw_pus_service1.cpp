a1e7317b833a131a80514525e27d18273f397048
Ignacio Vivas <nacho.vivas.abad@gmail.com>
2024-04-03 17:27:15 +0200

    Práctica 4
    Proyecto con servicio PUS 20

--- asw_pus_3_x_rtems_project_2024_students/service_libraries/icuasw_pus_services/src/icuasw_pus_service1.cpp
+++ asw_pus_3_x_rtems_project_2024_students/service_libraries/icuasw_pus_services/src/icuasw_pus_service1.cpp
@@ -106,8 +106,13 @@ void PUSService1::TryTCAcceptation(CDTCHandler &tcHandler) {
 				//TC Classified as HK_FDIRTC
 				tcHandler.SetExecCtrlAsHK_FDIRTC();
 				break;
-			//TODO Accept TC[3,6] and TC[3,31]
-
+			//Accept TC[3,6] and TC[3,31]
+			case (6):
+				tcHandler.SetExecCtrlAsHK_FDIRTC();
+				break;
+			case (31):
+				tcHandler.SetExecCtrlAsHK_FDIRTC();
+				break;
 			default:
 				acceptationStatus = TCAcceptationSubTypeError;
 			}
